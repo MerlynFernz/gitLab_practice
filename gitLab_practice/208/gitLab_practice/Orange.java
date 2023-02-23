b79104d01b8665463d0304b32071903fb77faa9c
admin123 <admin123@Merlyn>
2023-02-24 09:00:33 +1400

    changes did

--- gitLab_practice/208/gitLab_practice/Orange.java
+++ gitLab_practice/208/gitLab_practice/Orange.java
@@ -8,10 +8,15 @@ String color;
 	{
 		this.color=color;
 	}
+	String getcolor()
+	{
+		return color;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Orange greenApple= new Orange("green");
+		String color=greenApple.getcolor();
 		System.out.println("color of apple is ");
 		
 
